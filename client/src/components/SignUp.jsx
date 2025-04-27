3663df39b4fab877956e60d1066eff50b694f0b4
shamunur <shamunur08@gmail.com>
2025-04-27 22:14:16 +0600

    commit

---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,37 @@
+// src/components/SignUp.jsx
+import { useState } from "react";
+import { signup } from "../services/authService";
+
+function SignUp() {
+  const [formData, setFormData] = useState({ username: "", email: "", password: "" });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await signup(formData);
+      console.log("Signup Successful:", res.data);
+      alert("Signup successful!");
+    } catch (error) {
+      console.error(error);
+      alert("Signup failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
