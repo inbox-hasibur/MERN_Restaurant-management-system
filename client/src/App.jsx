3b47d9add9e64c2b0c58a36d9e4639c59ad6ac09
Hasibur Rahman <inbox.hasibur.rahman@gmail.com>
2025-04-28 20:32:35 +0600

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-//import { Route, Routes } from 'react-router-dom'
-// import Home from './pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 
 
@@ -15,7 +15,11 @@ const App = () => {
       {showLogin?<LogInpopUp/>:null}
       <div className='app'>
         <Navbar setShowLogin={setShowLogin} />
-    
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
       </div>
     </>
   )
