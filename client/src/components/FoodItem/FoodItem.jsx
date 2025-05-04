2efe3ba33a68ba9e8cf45e1ee85334387c1b183c
sadia934 <sadiasikder1038@gmail.com>
2025-05-04 18:42:55 +0600

    commit

---
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FoodItem.css'
+
+const FoodItem = (id,name,price,description,image)=> {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FoodItem 
+foo
