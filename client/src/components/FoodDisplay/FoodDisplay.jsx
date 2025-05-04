a778ff6f193045647750225288ee1e085d199030
sadia934 <sadiasikder1038@gmail.com>
2025-05-04 18:48:52 +0600

    food

---
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,21 @@
+import React, { use } from 'react'
+import './FoodDisplay.css'
+import FoodItem from '../FoodItem/FoodItem'
+
+const FoodDisplay = (category)=> {
+  
+    const {food_list} = useContext(StoreContext)
+  
+    return (
+        <div className='food-display' id='food-display'>
+            <h2>Top dishes near you</h2>
+            <div className="food-display-list">
+                {food_list.map((item,index)=>{
+                    return <FoodItem key={index} id={item._id} name={item.name} descriotion={iteam.descriotion} price={item.price} image={item.image}/>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default FoodDisplay
