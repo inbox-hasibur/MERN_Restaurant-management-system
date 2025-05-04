12f9003350ce3cc5a5b6f1b91de2abf8b276f582
Hasibur Rahman <inbox.hasibur.rahman@gmail.com>
2025-05-04 22:14:28 +0600

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,13 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-
 import LoginPopup from './components/LoginPopup/LoginPopup'
 
-
 const App = () => {
-
-    const [showLogin,setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
 
   return (
     <>
-      {/* {showLogin?<LoginPopup/>:<></>} */}
       {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
       <div className='app'>
         <Navbar setShowLogin={setShowLogin} />
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,17 +4,16 @@ import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({setShowLogin}) => {
-
     const [menu,setMenu] = useState("home");
 
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <Link to="/" onClick={()=>setMenu("home")} className={menu=="home"?"active":""}>home</Link>
-        <a href='#' onClick={()=>setMenu("menu")} className={menu=="menu"?"active":""}>menu</a>
-        <a href='#' onClick={()=>setMenu("mobile-app")} className={menu=="mobile-app"?"active":""}>mobile-app</a>
-        <a href='#' onClick={()=>setMenu("contact-us")} className={menu=="contact-us"?"active":""}>contact us</a>
+        <Link to="/" onClick={()=>setMenu("home")} className={menu=="home"?"active":""}>Home</Link>
+        <a href='#' onClick={()=>setMenu("menu")} className={menu=="menu"?"active":""}>Menu</a>
+        <a href='#' onClick={()=>setMenu("find-us")} className={menu=="find-us"?"active":""}>Find-us</a>
+        <a href='#' onClick={()=>setMenu("contact-us")} className={menu=="contact-us"?"active":""}>Contact Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -22,7 +21,7 @@ const Navbar = ({setShowLogin}) => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+        <button onClick={()=>setShowLogin(true)}>Sign In</button>
       </div>
     </div>
   );
