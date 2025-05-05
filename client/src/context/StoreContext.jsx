f0928c5daf486960db9b93bd46c89cad2f466e0f
Hasibur Rahman <inbox.hasibur.rahman@gmail.com>
2025-05-05 07:27:39 +0600

    Update StoreContext.jsx

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -1,20 +1,31 @@
-import { createContext } from "react";
-import { food_list } from "../assets/assets";
+import { createContext, useState } from "react"
+import { food_list } from "../assets/assets"
+
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
+    const [cart, setCart] = useState([])
     
-    
+    const addToCart = (item) => {
+        setCart([...cart, item])
+    }
+
+    const removeFromCart = (itemId) => {
+        setCart(cart.filter(item => item.id !== itemId))
+    }
     
     const contextValue = {
-            food_list
+        food_list,
+        cart,
+        addToCart,
+        removeFromCart
     }
+
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
     )
 }
-
 
 export default StoreContextProvider
