bee712a355ce50633d537d721fdd7aafbaf279fb
Hasibur Rahman <inbox.hasibur.rahman@gmail.com>
2025-05-28 00:40:12 +0600

    commit

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -3,53 +3,104 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import validator from 'validator';
 
-
-// login user
-const loginUser = async (req, res) => {
-
-}
-
 const createToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET)
+    return jwt.sign({id}, process.env.JWT_SECRET)
 }
 
-// Register user
-const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
+const loginUser = async (req, res) => {
+    const {email, password} = req.body
     try {
-        // Cheacking user already exists or not
-        const exists = await userModel.findOne({ email });
-        if (exists) {
-            return res.json({ success: false, message: 'User already exists' });
+        const user = await userModel.findOne({email})
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User does not exist"
+            })
         }
 
-        // Validating email
-        if (!validator.isEmail(email)) {
-            return res.json({ success: false, message: 'Invalid email format' });
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            })
         }
 
-        if(password.length < 8) {
-            return res.json({ success: false, message: 'Password is too short!' });
-        }
-
-        // Hashing password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const user = new userModel({
-            name:name,
-            email:email,
-            password:hashedPassword,
-        });
-        
-        const user = await user.save();
         const token = createToken(user._id)
-        res.json({success: true, token});
-    }
-    catch (error) {
-        console.log(error);
-        return res.json({ success: false, message: 'Error' });
+        res.status(200).json({
+            success: true,
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Server error",
+            error: error.message
+        })
     }
 }
 
-export { loginUser, registerUser };
+const registerUser = async (req, res) => {
+    const {name, password, email} = req.body
+    try {
+        const existingUser = await userModel.findOne({email})
+        
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Please enter a valid email"
+            })
+        }
+
+        if (password.length < 8) {
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 8 characters"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save()
+        const token = createToken(user._id)
+        
+        res.status(201).json({
+            success: true,
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Server error",
+            error: error.message
+        })
+    }
+}
+
+export {loginUser, registerUser}
